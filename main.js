2dcbdcf2ffe3ced2ec3f7444dbc143f1f7bbd4e8
PSD <psdubowik@gmail.com>
2017-10-30 23:34:27 +0100

    Checking if input is of array type

--- main.js
+++ main.js
@@ -9,30 +9,34 @@ function getQuote(){
 }
 
 function createTweet(input){
+    // console.log(input.typeof, input.length)
     var data = input[0];
 
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;
+    if (Array.isArray(input)){
 
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
-    }
+        var quoteText = $(data.content).text().trim();
+        var quoteAuthor = data.title;
+    
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+        }
+    
+        var tweetText = "Quote of the day - " + quoteText + "Author: " + quoteAuthor;
 
-    var tweetText = "Quote of the day - " + quoteText + "Author: " + quoteAuthor;
-
-    if (tweetText.length > 140){
-        getQuote();
-    } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-        $(".quote").text(quoteText);
-        $(".author").text("Author: " + quoteAuthor);
-        $(".tweet").attr("href", tweet);
+        if (tweetText.length > 140){
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            $(".quote").text(quoteText);
+            $(".author").text("Author: " + quoteAuthor);
+            $(".tweet").attr("href", tweet);
+        };
     };
-}
+};
 
 $(document).ready(function() {
     getQuote();
     $(".trigger").on("click", function() {
         getQuote();
-    })
-});
+    });
+})
